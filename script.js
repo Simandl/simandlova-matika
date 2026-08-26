8002fc7d83597f99a017ffd9e0e64dc5ca31c131
Simandl <simandl@gymtce.cz>
2026-08-26 23:31:52 +0200

    Update challenge questions and refactor event listener

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const vyzvy = [
-    "Kolik různých PINů lze vytvořit ze čtyř číslic?",
-    "Jaká je šance, že dva spolužáci mají stejné narozeniny?",
-    "Dokážeš najít tajné číslo pomocí 10 otázek?",
-    "Kolik hesel dlouhých 8 znaků lze vytvořit?",
-    "Napiš program pro výpočet délky jména."
+    "Kolika otázkami dokážeš najít tajné tříciferné číslo?",
+    "Jaká je šance, že dva studenti mají stejné narozeniny?",
+    "Kolik různých PINů lze vytvořit ze 4 číslic?",
+    "Napiš program, který spočítá počet samohlásek ve jméně.",
+    "Dokážeš rozdělit 20 studentů do čtyř skupin po pěti?",
+    "Jaká je pravděpodobnost, že si sedneš vedle svého nejlepšího kamaráda?"
 ];
 
-const btn = document.getElementById("challengeBtn");
-const text = document.getElementById("challengeText");
-
-btn.addEventListener("click", () => {
+document.getElementById("challengeBtn")
+.addEventListener("click", function() {
 
     const index =
         Math.floor(Math.random() * vyzvy.length);
 
-    text.innerHTML = vyzvy[index];
-
+    document.getElementById("challengeText")
+        .innerText = vyzvy[index];
 });
